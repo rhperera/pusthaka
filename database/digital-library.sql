a4c74d7f78391bd3288ece7f69df22c715b6a551
Anuradha Edirisuriya <anuradhaedirisuriya@gmail.com>
2014-09-03 14:02:32 +0530

    changed library and htaccess

--- database/digital-library.sql
+++ database/digital-library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 24, 2014 at 03:39 PM
+-- Generation Time: Sep 03, 2014 at 07:00 AM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
