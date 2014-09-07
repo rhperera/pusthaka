68ff0a94dd7da70c2e889417b41ac260690e1fb3
Anuradha Edirisuriya <anuradhaedirisuriya@gmail.com>
2014-09-07 11:42:30 +0530

    changed the interfaces

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,12 +1,64 @@
 <footer>
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; Your Website 2014</p>
-        </div>
-    </div>
-</footer>
+
+<div class="row">
+<div class="input-group">
+<div class="well">
+
+		<!-- Blog Categories Well -->
+		<div class="col-md-3 col-sm-6">
+			<h4>Categories</h4>
+			<div class="row">
+				<div class="col-lg-6">
+					<ul class="list-unstyled">
+
+						<?php if(!isset($categories))
+						{
+							echo '<p>No categories to show</p>';
+						}
+						else
+						{
+							foreach($categories as $row)
+							{
+								echo '<li><a href="';echo base_url(); echo 'categories/view/'; echo $row['category_id']; echo'">'; echo $row['category_name'];
+								echo '</a>
+						 </li>';
+							}
+						}
+						?>
+					</ul>
+				</div>
+				<!-- /.col-lg-6 -->
+			</div>
+			<!-- /.row -->
+		</div>
+
+
+		<!-- Side Widget Well -->
+		<div class="col-md-3 col-sm-6">
+			<h4>Side Widget Well</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+		</div>
+
+ 
+
+
+
+		<div class="row">
+			<div class="col-lg-12">
+				<p>Copyright &copy; Your Website 2014</p>
+			</div>
+		</div>
+		
+</div>	
 
 </div>
+</div>
+
+
+		
+</footer>
+
+
 <!-- /.container -->
 
 <!-- jQuery Version 1.11.0 -->
--- application/views/navbar.php
+++ application/views/navbar.php
@@ -52,8 +52,32 @@
                 }
                 ?>
             </ul>
+			
+		<!-- Blog Sidebar Widgets Column -->
+
+			        <div class="col-md-3 col-sm-6">
+
+            <!-- Blog Search Well -->
+  
+                <div class="input-group">
+                    <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                        </span>
+                </div>
+                <!-- /.input-group -->
+			</div>
+		
+		
         </div>
         <!-- /.navbar-collapse -->
+		
+
+		
     </div>
     <!-- /.container -->
+	
+	
+
+			
 </nav>
