13713a36a6fd45175381303b2ceaa56626b70cf3
wpalavanya <wpalavanya@gmail.com>
2014-09-10 11:48:50 +0530

    added about page

---
+++ application/controllers/about.php
@@ -0,0 +1,10 @@
+<?php
+
+class About extends CI_Controller{
+
+	function the_library(){
+		$this->load->view('about');
+	}
+
+	
+}
---
+++ application/views/about.php
@@ -0,0 +1,31 @@
+<?php 
+
+include('header.php');
+include('navbar.php'); ?>
+
+
+
+<h1><b>The Library</b></h1>
+
+<p> The UCSC Digital Library provides its service to the staff and students of the UCSC. 
+	There are more than 1,400 registered digital library members. 
+	The digital library hosts more than 10,000 books and has a 
+	comprehensive collection of computer science books 
+	supported by the latest editions and books related to Statistics. 
+	At the digital library members can refer to books, journals, reports, research papers 
+	dissertations and other resources such as magazines, newspapers 
+	such as  audio and video while accessing the internet in order to gather and update knowledge.
+</p>
+	
+<h1><b>Objectives</b></h1>	
+<p>Let authorized users to access to any authorized reading material any time of the day.</p>
+<p>Provide easy browsing and access.</p>
+<p>Librarian or the authorized users who has the upload permissions can upload new materials to the system.</p>
+<p>Share knowledge with students inside the university.</p>
+<p>Owner has the permission to share materials uploaded by himself.</p>
+<p>Some materials will be open for all the users while some materials will be public or private based on the permissions given to the users.</p>
+
+
+<?php 
+
+include('footer.php'); ?>
