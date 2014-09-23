116c3bc56aadab91c9ab3c37b37930f592d028d9
rhperera <harith9127@gmail.com>
2014-09-23 21:41:41 +0530

    added material class

---
+++ application/libraries/Material.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Material
+{
+	private $name;
+	private $author;
+	private $path;
+	private $status;
+	private $upload_date;
+	private $uploader_id;
+
+	public function __construct($name,$author,$uploader_id,$upload_date,$path,$status)
+	{
+		
+		$this->name=$name;
+		$this->author=$author;
+		$this->path=$path;
+		$this->status=$status;
+		$this->upload_date=$upload_date;
+		$this->uploader_id=$uploader_id;
+	}
+
+	public function get_name()
+	{
+		return $this->name;
+	}
+
+	public function get_author()
+	{
+		return $this->author;
+	}
+
+	public function get_path()
+	{
+		return $this->path;
+	}
+
+	public function get_upload_date()
+	{
+		return $this->upload_date;
+	}
+
+	public function get_uploader_id()
+	{
+		return $this->uploader_id;
+	}
+
+	public function get_status()
+	{
+		return $this->status;
+	}
+
+	public function get_data()
+	{
+		echo "string";
+	}
+}
+?>
