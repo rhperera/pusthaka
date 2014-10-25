ee13b2b104a6052cd34db8f22e34b1f32714b3dd
rhperera <harith9127@gmail.com>
2014-10-25 21:14:02 +0530

    more changes in the front end welcome page

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,21 @@
-
+<style>
+     #con
+    {
+        background-image:url('/ucsc-digital-library/assets/images/slider.jpg'); 
+        background-size: cover;
+    }
+</style>
 <!-- Navigation -->
 <?php //include('slider.php')?>
 <!-- Page Content -->
-<div class="container">
-    <br><br
-    <!-- Page Heading/Breadcrumbs -->
- <div class="row">
-        <div class="col-lg-4">
-        </div>
-        <div class="col-lg-4">
-            <h1 style="font-size: 43px; color: #fff; text-align: center">WELCOME<br><small>Digial Library of UCSC</small></h1>
-        </div>
-        <div class="col-lg-4">
-        </div>
-    </div> 
-<br><br>
-    <?php include('panel.php');?>
+<div id="con">
+    <div class="container">
 
-    
+        <?php include('panel.php');?>
+
+    </div>
+</div>
+<div class="container">
 
     <!-- /.row -->
 
--- application/views/panel.php
+++ application/views/panel.php
@@ -7,11 +7,7 @@
         color: #fff;
     }
 
-    body
-    {
-        background-image:url('/ucsc-digital-library/assets/images/slider.jpg'); 
-        background-size: cover;
-    }
+   
       
     
 </style>
@@ -21,33 +17,48 @@
 
       
         $("#read").mouseover(function () {
-            $("#advance1").stop(true, true).slideUp();
-            $("#upload1").stop(true, true).slideUp();
-            $("#collection1").stop(true, true).slideUp();
-            $("#read1").slideDown();
+            $("#advance1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#upload1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#collection1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#read1").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 200);
         });
-        $("#advance").hover(function () {
-            $("#advance1").slideDown();
-            $("#upload1").stop(true, true).slideUp();
-            $("#collection1").stop(true, true).slideUp();
-            $("#read1").stop(true, true).slideUp();
+        $("#advance").mouseover(function () {
+            $("#advance1").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 200);
+            $("#upload1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#collection1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#read1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
         });
-        $("#upload").hover(function () {
-            $("#advance1").stop(true, true).slideUp();
-            $("#upload1").slideDown();
-            $("#collection1").stop(true, true).slideUp();
-            $("#read1").stop(true, true).slideUp();
+        $("#upload").mouseover(function () {
+            $("#advance1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#upload1").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 200);
+            $("#collection1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#read1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
         });
-        $("#collection").hover(function () {
-            $("#advance1").stop(true, true).slideUp();
-            $("#upload1").stop(true, true).slideUp();
-            $("#collection1").slideDown();
-            $("#read1").stop(true, true).slideUp();
+        $("#collection").mouseover(function () {
+            $("#advance1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#upload1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
+            $("#collection1").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 200);
+            $("#read1").css({opacity: 0.0, visibility: "visible"}).stop(true, true).animate({opacity: 0.0}, 200);
         });
 
     });
 </script>
-<div class="row" id="panel">
+
+
+
+    <br><br>
+    <!-- Page Heading/Breadcrumbs -->
+ <div class="row">
+        <div class="col-lg-4">
+        </div>
+        <div class="col-lg-4">
+            <h1 style="font-size: 43px; color: #fff; text-align: center">WELCOME<br><small>Digial Library of UCSC</small></h1>
+        </div>
+        <div class="col-lg-4">
+        </div>
+    </div> 
+<br><br>
+<div class="row">
     </br>
     <div class="col-md-3 col-sm-6">
         <div class="panel panel-default text-center" style=" background-color: transparent !important; border-color: transparent">
@@ -57,13 +68,11 @@
                               <i class="fa fa-book fa-stack-1x fa-inverse"></i>
                         </span>
             </div>
-            <div class="panel-body" style=" background-color: transparent !important">
-                <div class="panel-body" style=" background-color: transparent !important; display:none" id="read1">
+                <div class="panel-body" style=" background-color: transparent !important; visibility: hidden" id="read1">
                     <h4>Read Online</h4>
                     <p>View and read most recently uploaded books now.</p>
                     <a href="<?php echo base_url()?>posts/recent" class="btn btn-primary">Recent Books</a>
                 </div>
-            </div>
         </div>
     </div>
     <div class="col-md-3 col-sm-6" >
@@ -74,13 +83,11 @@
                               <i class="fa fa-search fa-stack-1x fa-inverse"></i>
                         </span>
             </div>
-            <div class="panel-body" style=" background-color: transparent !important;">
-                <div class="panel-body" style=" background-color: transparent !important; display: none" id="advance1">
+                <div class="panel-body" style=" background-color: transparent !important; visibility: hidden" id="advance1">
                      <h4>Search</h4>
                      <p>Use our Advance Search for a customized search</p>
                      <a href="#" class="btn btn-primary" style="background-color:#CA4252; border-color:#CA4252">Advance Search</a>
                 </div>
-            </div>
         </div>
     </div>
     <div class="col-md-3 col-sm-6">
@@ -91,13 +98,11 @@
                               <i class="fa fa-upload fa-stack-1x fa-inverse" ></i>
                         </span>
             </div>
-            <div class="panel-body" style=" background-color: transparent !important;">
-                <div class="panel-body" style=" background-color: transparent !important; display: none" id="upload1">
+                <div class="panel-body" style=" background-color: transparent !important; visibility: hidden" id="upload1">
                     <h4>Upload</h4>
                     <p>Upload and share books on your own.</p>
                     <a href="<?php echo base_url()?>uploads" class="btn btn-primary" style="background-color:#42CA68; border-color:#42CA68  ">Upload now</a>
                 </div>
-            </div>
         </div>
     </div>
     <div class="col-md-3 col-sm-6">
@@ -108,13 +113,12 @@
                               <i class="fa fa-folder-open fa-stack-1x fa-inverse"></i>
                         </span>
             </div>
-            <div class="panel-body" style=" background-color: transparent !important;">
-                <div class="panel-body" style=" background-color: transparent !important; display: none " id="collection1">
+                <div class="panel-body" style=" background-color: transparent !important; visibility: hidden " id="collection1">
                     <h4>Manage Collections</h4>
                     <p>Get your favourite books and add them to collections.</p>
                     <a href="#" class="btn btn-primary" style="background-color:#b050bb; border-color:#b050bb">My Collections</a>
                 </div>
-            </div>
         </div>
     </div>
 </div>
+
