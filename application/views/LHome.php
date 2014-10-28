f8218d1d75829e6a94547b1865b29ccc309b5a54
anuradhaedirisuriya <anuradhaedirisuriya@gmail.com>
2014-10-28 13:11:04 +0530

    changed content of sidebar

--- application/views/LHome.php
+++ application/views/LHome.php
@@ -19,23 +19,9 @@
 
                 <div class="col-md-3">
                 <div class="list-group">
-                    <a href="index.html" class="list-group-item">Home</a>
-                    <a href="about.html" class="list-group-item">About</a>
-                    <a href="services.html" class="list-group-item">Services</a>
-                    <a href="contact.html" class="list-group-item">Contact</a>
-                    <a href="portfolio-1-col.html" class="list-group-item">1 Column Portfolio</a>
-                    <a href="portfolio-2-col.html" class="list-group-item">2 Column Portfolio</a>
-                    <a href="portfolio-3-col.html" class="list-group-item">3 Column Portfolio</a>
-                    <a href="portfolio-4-col.html" class="list-group-item">4 Column Portfolio</a>
-                    <a href="portfolio-item.html" class="list-group-item">Single Portfolio Item</a>
-                    <a href="blog-home-1.html" class="list-group-item">Blog Home 1</a>
-                    <a href="blog-home-2.html" class="list-group-item">Blog Home 2</a>
-                    <a href="blog-post.html" class="list-group-item">Blog Post</a>
-                    <a href="full-width.html" class="list-group-item">Full Width Page</a>
-                    <a href="sidebar.html" class="list-group-item active">Sidebar Page</a>
-                    <a href="faq.html" class="list-group-item">FAQ</a>
-                    <a href="404.html" class="list-group-item">404</a>
-                    <a href="pricing.html" class="list-group-item">Pricing Table</a>
+                    <a href="index.html" class="list-group-item">Dashboard</a>
+                    <a href="about.html" class="list-group-item">Approved Books</a>
+
                 </div>
             </div>
 
@@ -47,27 +33,21 @@
                             </div>
                             <div class="panel-body">
                                 <div class="list-group">
-                                    <a href="<?php echo base_url(); echo 'Lpanel/inactives'?>" class="list-group-item">
-                                        
-                                        <i class="fa fa-fw fa-book"></i> Books not active
-                                        <span class="badge"><?php echo $inactives[0]['inactive_books']; ?></span>
-                                    </a>
-
                                     <a href="#" class="list-group-item">
                                         <span class="badge">4 minutes ago</span>
-                                        <i class="fa fa-fw fa-comment"></i> Commented on a post
+                                        <i class="fa fa-fw fa-comment"></i> Commented on "Harry Potter"
                                     </a>
                                     <a href="#" class="list-group-item">
                                         <span class="badge">23 minutes ago</span>
-                                        <i class="fa fa-fw fa-truck"></i> Order 392 shipped
+                                        <i class="fa fa-fw fa-star"></i> Gave 5 stars to "Harry Potter"
                                     </a>
                                     <a href="#" class="list-group-item">
                                         <span class="badge">46 minutes ago</span>
-                                        <i class="fa fa-fw fa-money"></i> Invoice 653 has been paid
+                                        <i class="fa fa-fw fa-book"></i> Uploaded a PDF "Harry Potter"
                                     </a>
                                     <a href="#" class="list-group-item">
                                         <span class="badge">1 hour ago</span>
-                                        <i class="fa fa-fw fa-user"></i> A new user has been added
+                                        <i class="fa fa-fw fa-user"></i> A new user has been registered
                                     </a>
                                     <a href="#" class="list-group-item">
                                         <span class="badge">2 hours ago</span>
@@ -75,11 +55,11 @@
                                     </a>
                                     <a href="#" class="list-group-item">
                                         <span class="badge">yesterday</span>
-                                        <i class="fa fa-fw fa-globe"></i> Saved the world
+                                        <i class="fa fa-fw fa-globe"></i> Added a new category
                                     </a>
                                     <a href="#" class="list-group-item">
-                                        <span class="badge">two days ago</span>
-                                        <i class="fa fa-fw fa-check"></i> Completed task: "fix error on sales page"
+                                        <span class="badge">4 minutes ago</span>
+                                        <i class="fa fa-fw fa-comment"></i> Commented on "Networks"
                                     </a>
                                 </div>
                                 <div class="text-right">
@@ -136,7 +116,3 @@
                     
       </div>
 
-                    
-
-       
-</div>
