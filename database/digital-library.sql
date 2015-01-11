6fba31bf8f7e126305b6a3800e64613886bc3909
rhperera <harith9127@gmail.com>
2015-01-12 01:11:06 +0530

    database added

--- database/digital-library.sql
+++ database/digital-library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 29, 2014 at 11:39 AM
+-- Generation Time: Jan 11, 2015 at 07:40 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -65,14 +65,18 @@ INSERT INTO `categories` (`category_id`, `category_name`) VALUES
 
 CREATE TABLE IF NOT EXISTS `materials` (
   `material_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(15) NOT NULL,
   `name` varchar(30) DEFAULT NULL,
   `author` varchar(30) DEFAULT NULL,
   `uploader_id` int(11) NOT NULL,
   `upload_date` date DEFAULT NULL,
   `path` varchar(100) DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
+  `description` text,
+  `tags` varchar(100) DEFAULT NULL,
+  `privacy` int(1) NOT NULL,
   PRIMARY KEY (`material_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -100,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(30) DEFAULT NULL,
   `user_type` enum('admin','librarian','user') DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `users`
@@ -109,7 +113,21 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`user_id`, `user_name`, `email`, `password`, `user_type`) VALUES
 (1, 'admin', 'admin@mail.com', 'admin', 'admin'),
 (2, 'user', 'user@user.com', 'user', 'user'),
-(3, 'librarian', 'librarian@librarian.com', 'librarian', 'librarian');
+(3, 'librarian', 'librarian@librarian.com', 'librarian', 'librarian'),
+(4, 'user1', 'user1', 'user1', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `view_results`
+--
+
+CREATE TABLE IF NOT EXISTS `view_results` (
+  `user_id` int(11) DEFAULT NULL,
+  `material_id` int(11) DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `material_id` (`material_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
@@ -119,8 +137,15 @@ INSERT INTO `users` (`user_id`, `user_name`, `email`, `password`, `user_type`) V
 -- Constraints for table `material_category`
 --
 ALTER TABLE `material_category`
-  ADD CONSTRAINT `fk_material_id` FOREIGN KEY (`material_id`) REFERENCES `materials` (`material_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
+  ADD CONSTRAINT `fk_material_id` FOREIGN KEY (`material_id`) REFERENCES `materials` (`material_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `view_results`
+--
+ALTER TABLE `view_results`
+  ADD CONSTRAINT `view_results_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `view_results_ibfk_2` FOREIGN KEY (`material_id`) REFERENCES `materials` (`material_id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
