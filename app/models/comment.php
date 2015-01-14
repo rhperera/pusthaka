cc743c857cb80c0be6d93df94ce12e1fb8a0dc04
wpalavanya <wpalavanya@gmail.com>
2015-01-14 17:59:24 +0530

    add method get_comment for models/comment.php

--- app/models/comment.php
+++ app/models/comment.php
@@ -9,6 +9,16 @@ class Comment
         $this->db = $db;
         //var_dump(db);
     }
+	
+	function get_comment($comment_id)
+    {
+        $query = $this->db->query("
+        SELECT *
+        FROM comments
+        where comment_id = '$comment_id'"
+		);
+        return $query->fetchAll();
+    }
 }
 
 ?>
