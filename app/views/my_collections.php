37e6a6ba7fa5093885e07f747145e4e98cc55a80
anuradhaedirisuriya <anuradhaedirisuriya@gmail.com>
2015-01-19 13:48:48 +0530

    changed collections

--- app/views/my_collections.php
+++ app/views/my_collections.php
@@ -1,87 +1,143 @@
 <div class="container">
   
- <div class="row row-centered">
 
-  <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <?php  if(isset($_SESSION['user_name']) and $_SESSION['user_type']=='user') {?>
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav" style="margin-left:7.83%; margin-top:14.5%; left:0; top:0; width:18%; z-index:1000">
-                  <li>
-                        <a href="<?php echo ASSET_PATH;?>/mytable"><i class="fa fa-fw fa-dashboard"></i>My Table</a>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/mytable"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/collections"><i class="fa fa-fw fa-table"></i>My Collections</a>
+                        <a href="<?php echo ASSET_PATH;?>/collections"><i class="fa fa-fw fa-table"></i> My Collections</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i>Browse</a>
+                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i> Browse</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i>Search</a>
+                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i> Search</a>
                     </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i> Upload</a>
+                    </li>                    
                     <li >
-                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i>Settings</a>
+                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i> Settings</a>
                     </li>
-                    <li>
-                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i>Upload</a>
-                    </li>
+
 
                 </ul>
             </div>
+    <?php } elseif(isset($_SESSION['user_name']) and $_SESSION['user_type']=='librarian'){?>
+              <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav" style="margin-left:7.83%; margin-top:14.5%; left:0; top:0; width:18%; z-index:1000">
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/lpanel"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/usermanager"><i class="fa fa-fw fa-table"></i> User Manager</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i> Browse</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i> Search</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i> Upload</a>
+                    </li>                    
+                    <li >
+                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i> Settings</a>
+                    </li>
 
-  <div class="col-md-3 col-centered" style="left: 69px;">
-    
-  </div>
 
-<div class="col-md-4">
-  <ul>
-<?php
+                </ul>
+            </div> <?php }?>
 
-	foreach($data['records'] as $rec){
-      echo '<h4><span><li><a style="display: -webkit-inline-box;" href="';
-      echo ASSET_PATH.'/collections/books/'.$rec['collection_id'];
-      echo '"><p>';
-		  echo $rec['collection_name']."";
-      echo '</p></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'; 
-      echo ASSET_PATH.'/collections/delete_collection/'.$rec['collection_id'];
-      echo'"><i  class="fa fa-trash-o"></i></a></li></span></h4>';
-    }
-?>
-  </ul>
-</div>
-
-<div class="col-md-3">
-                <h3>Add a new collection</h3>
-                <form name="sentMessage" action="<?php echo ASSET_PATH;?>/collections/add_collection" method="post" id="contactForm" novalidate="">
-                    <div class="control-group form-group">
-                        <div class="controls">
-                            <label>Enter collection name</label>
-                            <input type="text" class="form-control" name="collection_name" required="" data-validation-required-message="">
-                            <p class="help-block"></p>
-                        </div>
+                    <div class="col-lg-12" style="text-align: center;">
+                    <h1 class="page-header">My Collection
+                        <small><?php echo $_SESSION['full_name'];?></small>
+                    </h1>
                     </div>
-                    <!-- For success/fail messages -->
-                    <button type="submit" class="btn btn-warning" style="padding: 6px 73px;">Add new collection</button>
-                </form>
+
+
+
+    <div class ="row">
+            <div class="col-md-3 col-centered" style="left: 69px;">
+                </br></br></br>
+            </div>
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Collections</h3>
+                </div>
+                
+                <div class="panel-body">
+
+
+                      
+                    <?php
+
+                    	foreach($data['records'] as $rec){
+                          echo '<h4><span><li><a style="display: -webkit-inline-box;" href="';
+                          echo ASSET_PATH.'/collections/books/'.$rec['collection_id'];
+                          echo '"><p>';
+                    		  echo $rec['collection_name']."";
+                          echo '</p></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'; 
+                          echo ASSET_PATH.'/collections/delete_collection/'.$rec['collection_id'];
+                          echo'"><i  class="fa fa-trash-o"></i></a></li></span></h4>';
+                        }
+                    ?>
+                      
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Add a new collection</h3>
+                </div>
+                
+                <div class="panel-body">
+
+                    <form name="sentMessage" action="<?php echo ASSET_PATH;?>/collections/add_collection" method="post" id="contactForm" novalidate="">
+                        <div class="control-group form-group">
+                            <div class="controls">
+                                <label>Enter collection name</label>
+                                <input type="text" class="form-control" name="collection_name" required="" data-validation-required-message="">
+                                <p class="help-block"></p>
+                            </div>
+                        </div>
+                        <!-- For success/fail messages -->
+                        <button type="submit" class="btn btn-warning" style="text-align:center;">Add new collection</button>
+                    </form>
                 <br>
 
-                <?php 
+                        <?php 
 
-                if(isset($_SESSION['collection_added']))
-                {
-                    if($_SESSION['collection_added']=="true")
-                        {       ?>
-                            <div class="alert alert-success">
-                                Successfully added
+                        if(isset($_SESSION['collection_added']))
+                        {
+                            if($_SESSION['collection_added']=="true")
+                                {       ?>
+                                    <div class="alert alert-success">
+                                        Successfully added
+                                    </div>
+                        <?php   }elseif ($_SESSION['collection_added']=="false") { ?>
+                            <div class="alert alert-danger">
+                                Something went wrong
                             </div>
-                <?php   }elseif ($_SESSION['collection_added']=="false") { ?>
-                    <div class="alert alert-danger">
-                        Something went wrong
-                    </div>
-                <?php }
+                        <?php }
 
-                unset($_SESSION['collection_added']);
+                        unset($_SESSION['collection_added']);
 
 
 
-            }    ?>
+                    }    ?>
                
+                 </div>
             </div>
+        </div>
 </div>
--- app/views/usermanager.php
+++ app/views/usermanager.php
@@ -67,37 +67,61 @@ $( document ).ready(function() {
 
 
 
-                 <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-               <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <?php  if(isset($_SESSION['user_name']) and $_SESSION['user_type']=='user') {?>
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav" style="margin-left:7.83%; margin-top:14.5%; left:0; top:0; width:18%; z-index:1000">
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/lpanel"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
+                        <a href="<?php echo ASSET_PATH;?>/mytable"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/usermanager"><i class="fa fa-fw fa-table"></i>User Manager</a>
+                        <a href="<?php echo ASSET_PATH;?>/collections"><i class="fa fa-fw fa-table"></i> My Collections</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i>Browse</a>
+                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i> Browse</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i>Search</a>
+                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i> Search</a>
                     </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i> Upload</a>
+                    </li>                    
                     <li >
-                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i>Settings</a>
+                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i> Settings</a>
                     </li>
-                    <li>
-                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i>Upload</a>
-                    </li>
+
 
                 </ul>
             </div>
+    <?php } elseif(isset($_SESSION['user_name']) and $_SESSION['user_type']=='librarian'){?>
+              <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav" style="margin-left:7.83%; margin-top:14.5%; left:0; top:0; width:18%; z-index:1000">
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/lpanel"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/usermanager"><i class="fa fa-fw fa-table"></i> User Manager</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i> Browse</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i> Search</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i> Upload</a>
+                    </li>                    
+                    <li >
+                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i> Settings</a>
+                    </li>
 
 
-                                <div class="col-lg-12" style="text-align: center;">
+                </ul>
+            </div> <?php }?>
+
+
 
                     <div class="col-lg-12" style="text-align: center;">
-
                     <h1 class="page-header">User Manager
                         <small><?php echo $_SESSION['full_name'];?></small>
                     </h1>
