88339c8b2672166280fceabcb94bb5f790ec8b00
anuradhaedirisuriya <anuradhaedirisuriya@gmail.com>
2015-01-19 13:27:19 +0530

    changed mytable

--- app/views/mytable.php
+++ app/views/mytable.php
@@ -1,28 +1,42 @@
+<style>
+     #con
+    {
+        background-image:url('<?php echo ASSET_PATH;?>/images/slider.jpg'); 
+        background-size: cover;
+
+        height:100%;
+        width:100%;
+    }
+</style>
+
+<div id="con">
+
 <div class="container">
 
 
 
                 <?php  if(isset($_SESSION['user_name']) and $_SESSION['user_type']=='user') {?>
-  <div class="collapse navbar-collapse navbar-ex1-collapse">
+  	<div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav" style="margin-left:7.83%; margin-top:14.5%; left:0; top:0; width:18%; z-index:1000">
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/mytable"><i class="fa fa-fw fa-dashboard"></i>My Table</a>
+                        <a href="<?php echo ASSET_PATH;?>/mytable"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/collections"><i class="fa fa-fw fa-table"></i>My Collections</a>
+                        <a href="<?php echo ASSET_PATH;?>/collections"><i class="fa fa-fw fa-table"></i> My Collections</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i>Browse</a>
+                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i> Browse</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i>Search</a>
+                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i> Search</a>
                     </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i> Upload</a>
+                    </li>                    
                     <li >
-                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i>Settings</a>
+                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i> Settings</a>
                     </li>
-                    <li>
-                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i>Upload</a>
-                    </li>
+
 
                 </ul>
             </div>
@@ -31,23 +45,24 @@
                     <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav" style="margin-left:7.83%; margin-top:14.5%; left:0; top:0; width:18%; z-index:1000">
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/lpanel"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
+                        <a href="<?php echo ASSET_PATH;?>/lpanel"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/usermanager"><i class="fa fa-fw fa-table"></i>User Manager</a>
+                        <a href="<?php echo ASSET_PATH;?>/usermanager"><i class="fa fa-fw fa-table"></i> User Manager</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i>Browse</a>
+                        <a href="<?php echo ASSET_PATH;?>/main/browse"><i class="fa fa-fw fa-search"></i> Browse</a>
                     </li>
                     <li>
-                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i>Search</a>
+                        <a href="<?php echo ASSET_PATH;?>/search/quick"><i class="fa fa-fw fa-search"></i> Search</a>
                     </li>
+                    <li>
+                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i> Upload</a>
+                    </li>                    
                     <li >
-                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i>Settings</a>
+                        <a href="<?php echo ASSET_PATH;?>/settings"><i class="fa fa-fw fa-edit"></i> Settings</a>
                     </li>
-                    <li>
-                        <a href="<?php echo ASSET_PATH;?>/uploads"><i class="fa fa-fw fa-upload"></i>Upload</a>
-                    </li>
+
 
                 </ul>
             </div> <?php }?>
@@ -109,8 +124,10 @@
 						<ul>
 							<?php foreach ($data['books'] as $key) 
 							{	?>
-							<li><a href="<?php echo ASSET_PATH?>/main/item/<?php echo $key['material_id']?>">
-								<?php echo $key['name'];?></a>&nbsp;by&nbsp;<?php echo $key['author'];?></li>
+							<li class="list-group-item">
+							<a href="<?php echo ASSET_PATH?>/main/item/<?php echo $key['material_id']?>">
+							<?php echo $key['name'];?></a>&nbsp;by&nbsp;<?php echo $key['author'];?>
+							</li>
 							<?php } ?>
 						</ul>
 				</ol>
