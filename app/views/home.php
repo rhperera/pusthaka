251d66a47849f236b4fd2058749b7189fc7c73be
anuradhaedirisuriya <anuradhaedirisuriya@gmail.com>
2015-01-20 20:54:23 +0530

    changed recent books ui

--- app/views/home.php
+++ app/views/home.php
@@ -61,8 +61,8 @@
     <div class="row" style="top:3%;">
         <div class="col-lg-4"></div>
         <div class="col-lg-4"></div>
-        <div class="col-lg-4" style="background-color:rgba(255, 255, 255, 0.8); text-align:justify; padding:0 20px 10px 20px; -webkit-border-radius: 0 0 10px 10px;">
-        <h3>UCSC Digital Library</h3>
+        <div class="col-lg-4" style="background-color:rgba(255, 255, 255, 0.8); text-align:justify; padding:0 20px 10px 20px; -webkit-border-radius: 0 0 10px 10px; -moz-border-radius: 0 0 10px 10px;">
+        <h3 style="text-align:center;">UCSC Digital Library</h3>
         <p>UCSC Digital Library (UCSC-DL) is an online e-Repository which mainly contains published materials of UCSC students and staff, namely undergraduate, masters and research level dissertations, thesis and published research papers. In addition, UCSC-DL contains electronic materials shared among internal staff. Only authorized personnel will be allowed to access library materials but selected materials as listed here, could be accessed without login into the system. This repository is updated regularly, and new works are added to collections on a continuous basis.</p>
         </div>
     </div>
@@ -70,8 +70,8 @@
 
 
 
-    <div class="container" style="bottom:5%; left: 0; right: 0; position:absolute; background-color:rgba(255, 255, 255, 0.25);">
-            <h3 style="color:#fff;">Recent Books</h3>
+    <div class="container" style="bottom:5%; left: 0; right: 0; position:absolute; background-color:rgba(255, 255, 255, 0.25); -webkit-border-radius: 10px 0 10px 0px;">
+            <div class="col-md-12"><h3 style="color:#fff;">Recent Books</h3></div>
 
 
                          <?php //include('panel.php');?>
