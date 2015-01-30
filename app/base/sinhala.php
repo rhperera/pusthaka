6fa6dc86cb484caadc3592f06d5c83e556536424
rhperera <harith9127@gmail.com>
2015-01-30 19:43:35 +0530

    languages added

---
+++ app/base/sinhala.php
@@ -0,0 +1,72 @@
+<?php
+
+// Language Sinhala
+
+$GLOBALS['lang']['title']="පුස්තක";
+
+//Navigation bar
+$GLOBALS['lang']['nav_header']="පුස්තකාලයේ නම";
+$GLOBALS['lang']['sign-in']="අැතූල් වන්න";
+$GLOBALS['lang']['sign-out']="පිටවන්න";
+$GLOBALS['lang']['forgot_password']="මුරපදය අමතකය";
+$GLOBALS['lang']['browse']="ගවේශනය";
+$GLOBALS['lang']['search']="සොයන්න";
+$GLOBALS['lang']['upload']="උඩුගත කරන්න";
+
+//Commonn in pages
+$GLOBALS['lang']['welcome']="අායුබෝවන්";
+$GLOBALS['lang']['name']="නම";
+$GLOBALS['lang']['email']="E-Mail";
+
+//Sidebar
+$GLOBALS['lang']['dasboard']="dashboard";
+$GLOBALS['lang']['user_manager']="User Manager";
+$GLOBALS['lang']['collections']="Collections";
+$GLOBALS['lang']['settings']="Settings";
+$GLOBALS['lang']['advanced_search']="Advanced Search";
+
+//Librarian Home
+$GLOBALS['lang']['activities']="Activities";
+$GLOBALS['lang']['total_books']="Total Books";
+$GLOBALS['lang']['new_books']="New Books";
+$GLOBALS['lang']['awaiting']="Awaiting Approval";
+$GLOBALS['lang']['total_users']="Total Users";
+$GLOBALS['lang']['banned_users']="Banned Users";
+
+//User Manager
+$GLOBALS['lang']['reg_number']="Registration Number";
+$GLOBALS['lang']['full_name']="Full Name";
+$GLOBALS['lang']['ban']="ban";
+$GLOBALS['lang']['authorize']="Authorize";
+
+//Browse
+$GLOBALS['lang']['recent_books']="Recent Books";
+$GLOBALS['lang']['browse_by_category']="Browse by Category";
+
+//Advanced search
+$GLOBALS['lang']['sort_search_by']="Sort search by";
+$GLOBALS['lang']['title_or_tag']="Title or tag";
+$GLOBALS['lang']['name_of_author']="Name of Author";
+$GLOBALS['lang']['search_results']="Search Results";
+
+//Uploads
+$GLOBALS['lang']['upload_a_book']="Uplaod a Book";
+$GLOBALS['lang']['isbn']="ISBN number";
+$GLOBALS['lang']['book_title']="Title of Book";
+$GLOBALS['lang']['author']="Author";
+$GLOBALS['lang']['category']="Category";
+$GLOBALS['lang']['short_description']="Short Description";
+$GLOBALS['lang']['is_public']="Do you want to make this book publicly available";
+$GLOBALS['lang']['yes']="Yes";
+$GLOBALS['lang']['no_let_me']="No. Let me choose";
+
+//Settings
+$GLOBALS['lang']['change_password']="Change Password";
+$GLOBALS['lang']['old_pass']="Old password";
+$GLOBALS['lang']['new_pass']="New password";
+$GLOBALS['lang']['re_enter_pass']="Re-enter password";
+
+$GLOBALS['lang']['add_user']="";
+$GLOBALS['lang']['user_name']="";
+$GLOBALS['lang']['password']="Password";
+$GLOBALS['lang']['batch_year']="Batch year";
